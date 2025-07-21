1c2fb3da0366b580c8ccab6d773a1a204db0c08e
Kenneth Andrade <kennethandradeom@gmail.com>
2025-07-20 22:04:51 -0500

    Apagado con ruta de archivo suspension

---
+++ ApagadoProgramado/apagado_programado.py
@@ -0,0 +1,31 @@
+from flask import Flask, request
+import os
+import platform
+import threading
+import time
+
+app = Flask(__name__)
+# Ejecucion que no bloquea al principal /hilo
+def ejecutar_accion(accion, delay):
+    time.sleep(delay)
+    if accion == "apagar":
+        os.system("shutdown /s /t 0")
+    elif accion == "suspender":
+        os.system(r'"FILES_APAGADO\PSTools\psshutdown.exe" -d -t 0')
+
+@app.route("/apagar")
+def apagar():
+    delay = int(request.args.get("t", 0))  # Ej: /apagar?t=45
+    threading.Thread(target=ejecutar_accion, args=("apagar", delay)).start()
+    return f"Apagado programado en {delay} segundos"
+
+@app.route("/suspender")
+def suspender():
+    delay = int(request.args.get("t", 0))
+    threading.Thread(target=ejecutar_accion, args=("suspender", delay)).start()
+    return f"Suspensión programada en {delay} segundos"
+
+if __name__ == "__main__":
+    app.run(port=4414)
+
+    
